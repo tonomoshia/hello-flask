0339107c3416a1ccadf3022e2a6544217d1c260c
Tonomoshia <tonomoshia@gmail.com>
2019-09-07 18:06:07 -0400

    add form html into global form variable

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from flask import Flask
+from flask import request
+
+
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
+form = """
+<!doctype html>
+<html>
+    <body>
+    <form>
+        <label for="first_name">First Name</label>
+        <input id="first_name" type="text" name="first_name" />
+        <input type="submit" />
+    </form>
+    </body>
+</html>
+"""
 @app.route("/")
 def index():
-    return """
-        <h1>Hello World</h1>
-    """
+    return form
 
 app.run()
