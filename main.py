07968935bf3297da25ac5c87df54c32ea1657cfe
Tonomoshia <tonomoshia@gmail.com>
2019-09-12 13:32:09 -0400

    added time query string and to successful response string with .format

--- main.py
+++ main.py
@@ -89,7 +89,8 @@ def validate_time():
             minutes = ''
 
     if not minutes_error and not hours_error:
-        return redirect('/valid-time')
+        time = str(hours) + ':' + str(minutes)
+        return redirect('/valid-time?time={0}'.format(time))
     else:
         return time_form.format(hours_error=hours_error,
                                 minutes_error=minutes_error,
@@ -99,7 +100,8 @@ def validate_time():
 
 @app.route('/valid-time')
 def valid_time():
-    return '<h1>Thanks for submitting a valid time!</h1>'
+    time = request.args.get('time')
+    return '<h1>You submitted {0}. Thanks for submitting a valid time!</h1>'.format(time)
 
 
 app.run()
