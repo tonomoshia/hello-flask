8e8cc90d365cf1e86d75cdf193673edf9ff8b2d6
Tonomoshia <tonomoshia@gmail.com>
2019-09-11 20:35:51 -0400

    add time_form global variable and display_time_form function

--- main.py
+++ main.py
@@ -26,4 +26,28 @@ def hello():
     return '<h1>Hello, ' + first_name + '</h1>'
 
 
+time_form = """
+    <style>
+        .error {( color: red; )}
+    <style>
+    <h1>Validate Time</h1>
+    <form method='POST'>
+        <label>Hours (24-hour format)
+            <input name="hours" type="text" value='{hours}' />
+        </label>
+        <p class="error">{hours_error}</p>
+        <label>Minutes
+            <input name="minutes" type="text" value='{minutes}' />
+        </label>
+        <p class="error">{minutes_error}</p>
+        <input type="submit" value="Validate" />
+    </form>
+    """
+
+@app.route('/validate-time')
+def display_time_form():
+    return time_form.format(hours='', hours_error=''
+    minutes='', minutes_error='')
+
+
 app.run()
