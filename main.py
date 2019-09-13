7e5da905e7f58de79a4a9f6f6a90502604e13600
Tonomoshia <tonomoshia@gmail.com>
2019-09-13 13:47:04 -0400

    tell jinja2 to escape user-inputted HTML with autoescape set to true

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import jinja2
 template_dir = os.path.join(os.path.dirname(__file__),
     'templates')
 jinja_env = jinja2.Environment(
-    loader = jinja2.FileSystemLoader(template_dir))
+    loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
