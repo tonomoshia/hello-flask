3d567470c1acdd366e70fc4cf806e926657c6152
Tonomoshia <tonomoshia@gmail.com>
2019-09-13 13:54:38 -0400

    refactor time and time validation form to use jinja2 template

--- main.py
+++ main.py
@@ -4,48 +4,32 @@ import os
 import jinja2
 
 template_dir = os.path.join(os.path.dirname(__file__),
-    'templates')
+                            'templates')
 jinja_env = jinja2.Environment(
-    loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+    loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
 
 @app.route("/")
 def index():
     template = jinja_env.get_template('hello_form.html')
     return template.render()
 
+
 @app.route("/hello", methods=['POST'])
 def hello():
     first_name = request.form['first_name']
     template = jinja_env.get_template('hello_greeting.html')
-    return template.render(name= first_name)
-
-
-time_form = """
-    <style>
-        .error {{ color: red; }}
-    </style>
-    <h1>Validate Time</h1>
-    <form method='POST'>
-        <label>Hours (24-hour format)
-            <input name="hours" type="text" value='{hours}' />
-        </label>
-        <p class="error">{hours_error}</p>
-        <label>Minutes
-            <input name="minutes" type="text" value='{minutes}' />
-        </label>
-        <p class="error">{minutes_error}</p>
-        <input type="submit" value="Validate" />
-    </form>
-    """
+    return template.render(name=first_name)
 
 
 @app.route('/validate-time')
 def display_time_form():
-    return time_form.format(hours='', hours_error='',
-                            minutes='', minutes_error='')
+    template = jinja_env.get_template('time_form.html')
+    # when using template, do not have to pass in strings manually
+    return template.render()
 
 
 def is_integer(num):
@@ -88,10 +72,11 @@ def validate_time():
         # return redirect(url-path) where the url-path is a handler that we create to display the HTML for a response to valid inputs.
         return redirect('/valid-time?time={0}'.format(time))
     else:
-        return time_form.format(hours_error=hours_error,
-                                minutes_error=minutes_error,
-                                hours=hours,
-                                minutes=minutes)
+        template = jinja_env.get_template('time_form.html')
+        return template.render(hours_error=hours_error,
+                               minutes_error=minutes_error,
+                               hours=hours,
+                               minutes=minutes)
 
 
 @app.route('/valid-time')
